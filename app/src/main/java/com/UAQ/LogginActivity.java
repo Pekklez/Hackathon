faf0e98d59c0f8b412e495f8c919f3613b8c440d
Jose Antonio Gonzalez Aguilar <pks-16@hotmail.com>
2016-11-06 00:26:04 -0600

    ADD Facebook Pic

--- app/src/main/java/com/UAQ/LogginActivity.java
+++ app/src/main/java/com/UAQ/LogginActivity.java
@@ -1,36 +1,42 @@
 package com.UAQ;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
-import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
 public class LogginActivity extends AppCompatActivity {
+
+    LoginButton loginButton;
     CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_loggin);
 
-        FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
-        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
 
+                String idFacebook = loginResult.getAccessToken().getUserId();
+
+
+
+                Intent intent = new Intent(LogginActivity.this, MainActivity.class);
+                intent.putExtra("idFacebook", idFacebook);
+                startActivity(intent);
             }
 
             @Override
@@ -39,38 +45,16 @@ public class LogginActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onError(FacebookException error) {
+            public void onError(FacebookException e) {
 
             }
-
         });
-
-        AppEventsLogger.activateApp(this);
-
-        LoginManager.getInstance().registerCallback(callbackManager,
-                new FacebookCallback<LoginResult>() {
-                    @Override
-                    public void onSuccess(LoginResult loginResult) {
-                        // App code
-                    }
-
-                    @Override
-                    public void onCancel() {
-                        // App code
-                    }
-
-                    @Override
-                    public void onError(FacebookException exception) {
-                        // App code
-                    }
-                });
-
-
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
+
--- app/src/main/java/com/UAQ/MainActivity.java
+++ app/src/main/java/com/UAQ/MainActivity.java
@@ -1,37 +1,59 @@
 package com.UAQ;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
+    ImageView imageViewFacebook_logged;
+    Drawable imagen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.acitivity_principal);
+
+        //Get Image
+
+        String idFacebook = getIntent().getExtras().getString("idFacebook");
+        new getUserPicFB(imagen).execute("https://graph.facebook.com/"+idFacebook+"/picture?type=large");
 
        // setToolbar();
 
@@ -79,6 +101,7 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.openDrawer, R.string.closeDrawer){
 
+
             @Override
             public void onDrawerClosed(View drawerView) {
                 // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
@@ -99,6 +122,13 @@ public class MainActivity extends AppCompatActivity {
         //calling sync state is necessay or else your hamburger icon wont show up
         actionBarDrawerToggle.syncState();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
 
     private void setToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -139,4 +169,51 @@ public class MainActivity extends AppCompatActivity {
             return mFragmentTitles.get(position);
         }
     }
+
+
+
+    //Get User pick from facebook
+    class getUserPicFB extends AsyncTask<String, Void, Bitmap> {
+        Drawable imagen;
+
+        public getUserPicFB(Drawable image){
+            this.imagen = image;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+
+            int width = result.getWidth();
+            int height = result.getHeight();
+            int newW = 200;
+            int newH = 200;
+            float scaleW = ((float)newW)/width;
+            float scaleH = ((float)newH)/height;
+            Matrix x = new Matrix();
+            x.postScale(scaleW,scaleH);
+            Bitmap resized = Bitmap.createBitmap(result,0,0,width,height,x,true);
+            RoundedBitmapDrawable roundedDrawable = RoundedBitmapDrawableFactory.create(Resources.getSystem() , resized);
+            roundedDrawable.setCornerRadius(resized.getHeight());
+            imagen = roundedDrawable;
+
+            imageViewFacebook_logged = (ImageView)findViewById(R.id.profile_image);
+            imageViewFacebook_logged.setImageDrawable(imagen);
+
+        }
+    }
+
+
+
 }
